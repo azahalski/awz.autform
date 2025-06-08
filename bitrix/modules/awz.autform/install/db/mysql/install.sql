2d1780112bcf05afe3fb1e978045210dec7e48d2
Andrew Zahalski <andrew@zahalski.dev>
2025-06-08 17:10:00 +0300

    - ошибка создания таблицы лога pgsql;

--- bitrix/modules/awz.autform/install/db/mysql/install.sql
+++ bitrix/modules/awz.autform/install/db/mysql/install.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `b_awz_autform_codes` (
 CREATE TABLE IF NOT EXISTS `b_awz_autform_logs` (
     ID int(18) NOT NULL AUTO_INCREMENT,
     PHONE varchar(255) NOT NULL,
-    CREATE_DATE datetime NOT NULL
+    CREATE_DATE datetime NOT NULL,
     PRM longtext,
     PRIMARY KEY (`ID`)
 );
--- bitrix/modules/awz.autform/install/version.php
+++ bitrix/modules/awz.autform/install/version.php
@@ -1,5 +1,5 @@
 <?
 $arModuleVersion = array(
-    "VERSION" => "2.0.2",
-    "VERSION_DATE" => "2025-06-08 16:38:00"
+    "VERSION" => "2.0.3",
+    "VERSION_DATE" => "2025-06-08 17:09:00"
 );
