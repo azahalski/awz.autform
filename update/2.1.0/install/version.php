63092a36fc3beb6c6eb65680cb92b6e2bd227893
Andrew Zahalski <andrew@zahalski.dev>
2025-06-13 23:54:53 +0300

    gulp checkpoint

---
+++ update/2.1.0/install/version.php
@@ -0,0 +1,5 @@
+<?
+$arModuleVersion = array(
+    "VERSION" => "2.1.0",
+    "VERSION_DATE" => "2025-06-13 23:51:00"
+);
---
+++ update/2.1.0/lang/ru/install/index.php
@@ -0,0 +1,8 @@
+<?
+$MESS["AWZ_AUTFORM_MODULE_NAME"] = "AWZ: Форма авторизации";
+$MESS["AWZ_AUTFORM_MODULE_DESCRIPTION"] = "Модуль Для организации авторизации и регистрации по смс кодам";
+$MESS["AWZ_PARTNER_NAME"] = "Andrew Zahalski";
+$MESS["AWZ_AUTFORM_INSTALL_TITLE"] = "Удаление модуля AWZ: Форма авторизации";
+$MESS['MOD_UNINST_SAVE_OPTS'] = "Вы можете сохранить основные настройки модуля";
+$MESS['MOD_UNINST_SAVE_EVENTS_DESC'] = "Вы можете сохранить почтовые шаблоны";
+$MESS['MOD_UNINST_SAVE_OPTS_LABEL'] = "Сохранить настройки";
